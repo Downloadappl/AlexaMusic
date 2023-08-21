0d8a0daf6c2179a005c45bed3c753926015c6c97
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-08-21 16:33:31 +0300

    Update start.py

--- AlexaMusic/utils/inline/start.py
+++ AlexaMusic/utils/inline/start.py
@@ -21,27 +21,27 @@ def start_pannel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
+                text=_["‹ : الاوامر : ›"],
                 url=f"https://t.me/{app.username}?start=help",
             ),
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
+            InlineKeyboardButton(text=_[""], callback_data="settings_helper"),
         ],
     ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
         buttons.append(
             [
-                InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
+                InlineKeyboardButton(text=_["‹ : قناة الشروحات : ›"], url=f"{SUPPORT_CHANNEL}"),
+                InlineKeyboardButton(text=_["‹ : سورس عين : ›"], url=f"{SUPPORT_GROUP}"),
             ]
         )
     else:
         if SUPPORT_CHANNEL:
             buttons.append(
-                [InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
+                [InlineKeyboardButton(text=_["‹ : قناة الشروحات : ›"], url=f"{SUPPORT_CHANNEL}")]
             )
         if SUPPORT_GROUP:
             buttons.append(
-                [InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
+                [InlineKeyboardButton(text=_["‹ : سورس عين : ›"], url=f"{SUPPORT_GROUP}")]
             )
     return buttons
 
@@ -53,23 +53,23 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
         buttons.append(
             [
-                InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
+                InlineKeyboardButton(text=_["‹ : قناة الشروحات : ›"], url=f"{SUPPORT_CHANNEL}"),
+                InlineKeyboardButton(text=_["‹ : سورس عين : ›"], url=f"{SUPPORT_GROUP}"),
             ]
         )
     else:
         if SUPPORT_CHANNEL:
             buttons.append(
-                [InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
+                [InlineKeyboardButton(text=_["‹ : قناة الشروحات : ›"], url=f"{SUPPORT_CHANNEL}")]
             )
         if SUPPORT_GROUP:
             buttons.append(
-                [InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
+                [InlineKeyboardButton(text=_["‹ : سورس عين : ›"], url=f"{SUPPORT_GROUP}")]
             )
     buttons.append(
         [
             InlineKeyboardButton(
-                text=_["S_B_5"],
+                text=_["‹ : اضف البوت الى مجموعتك : ›"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ]
@@ -77,22 +77,22 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     if GITHUB_REPO and OWNER:
         buttons.append(
             [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+                InlineKeyboardButton(text=_["‹ : المطور : ›"], user_id=OWNER),
+                InlineKeyboardButton(text=_["‹ : لتنصيب بوت : ›"], url=f"{GITHUB_REPO}"),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+                    InlineKeyboardButton(text=_[""], url=f"{GITHUB_REPO}"),
                 ]
             )
         if OWNER:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
+                    InlineKeyboardButton(text=_["‹ : المطور : ›"], user_id=OWNER),
                 ]
             )
-    buttons.append([InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")])
+    buttons.append([InlineKeyboardButton(text=_["‹ : لغه : ›"], callback_data="LG")])
     return buttons
