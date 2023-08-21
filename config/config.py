2589aa00dec3a1c8178e09ff8e3ffc3db6fa35f5
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-08-21 16:23:58 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -44,8 +44,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alexa_BotUpdates")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Alexa_Help")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ZZZ7iZ")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ZZZ7iZ")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
