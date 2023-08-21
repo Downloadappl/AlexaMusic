285354e41315ffec32f31ea52cfc39b4816cb54b
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-08-21 23:14:53 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -44,8 +44,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ZZZ7iZ")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/H_M_Dr/25")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/H_M_Dr/25")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ZZZ7iZ")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
