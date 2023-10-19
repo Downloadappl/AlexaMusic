b6d17b30e1e0e8a2884706851537f0d543fc4bd4
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-10-19 14:20:48 +0300

    Update join.py

--- AlexaMusic/plugins/sudo/join.py
+++ AlexaMusic/plugins/sudo/join.py
@@ -1,8 +1,8 @@
-from pyrogram import Client, filters
+AlexaMusicfrom pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from config import SUPPORT_IQ
-from YukkiMusic import app
+from AlexaMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
