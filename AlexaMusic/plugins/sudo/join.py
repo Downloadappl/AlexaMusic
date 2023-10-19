e3fa7af5b1c54adc5ff407718270290c5ea3694b
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-10-19 20:26:43 +0300

    Update join.py

--- AlexaMusic/plugins/sudo/join.py
+++ AlexaMusic/plugins/sudo/join.py
@@ -2,22 +2,26 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from AlexaMusic import app
+from config import SUPPORT_ZZ
+
+
+
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not SUPPORT_IQ:  # Not compulsory
+    if not SUPPORT_ZZ:  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member(SUPPORT_IQ, msg.from_user.id)
+            await bot.get_chat_member(SUPPORT_ZZ, msg.from_user.id)
         except UserNotParticipant:
-            if SUPPORT_IQ.isalpha():
-                link = "https://t.me/" + SUPPORT_IQ
+            if SUPPORT_ZZ.isalpha():
+                link = "https://t.me/" + SUPPORT_ZZ
             else:
-                chat_info = await bot.get_chat(SUPPORT_IQ)
+                chat_info = await bot.get_chat(SUPPORT_ZZ)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"**⚠️ | عزيزي المشترك عليك الاشتراك في قنـاة البوت @ZZZ7iZ • تأكد من الاشتراك ثم اضغط /start ✅**",
+                    f"⚠️ | عزيزي المشترك عليك الاشتراك في قنـاة البوت @ZZZ7iZ • تأكد من الاشتراك ثم اضغط /start ✅",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("‹ اضغط هنا ›", url=link)]
@@ -27,4 +31,4 @@ async def must_join_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat : {SUPPORT_IQ} !")
+        print(f"I'm not admin in the MUST_JOIN chat : {SUPPORT_ZZ} !")
