2839800869fc7243eb70dc7fff28947cbf6f2e8c
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-10-22 02:00:22 +0300

    Update and rename تاغ.py to تاك.py

--- AlexaMusic/plugins/play/تاك.py
+++ AlexaMusic/plugins/play/تاك.py
@@ -1,6 +1,5 @@
 import asyncio
 import os
-from config import OWNER_ID
 import time
 import requests
 import aiohttp
@@ -14,21 +13,24 @@ from asyncio import gather
 from pyrogram.errors import FloodWait
 
 
-@app.on_message(command(["المالك"]) & filters.group)
+
+
+
+@app.on_message(command(["المالك", "صاحب الخرابه", "المنشي"]) & filters.group)
 async def gak_owne(client: Client, message: Message):
       if len(message.command) >= 2:
          return 
       else:
             chat_id = message.chat.id
-          
-            async for member in client.get_chat_members(chat_id):
-               if member.status == ChatMemberStatus.OWNER:
+            f = "administrators"
+            async for member in client.iter_chat_members(chat_id, filter=f):
+               if member.status == "creator":
                  id = member.user.id
                  key = InlineKeyboardMarkup([[InlineKeyboardButton(member.user.first_name, user_id=id)]])
                  m = await client.get_chat(id)
                  if m.photo:
                        photo = await app.download_media(m.photo.big_file_id)
-                       return await message.reply_photo(photo, caption=f"**✧ ¦ معلومات مالك الكروب \n\n ✧ ¦ اسمه : {m.first_name} \n ✧ ¦ معرفه : @{m.username} \n ✧ ¦ البايو : {m.bio}**",reply_markup=key)
+                       return await message.reply_photo(photo, caption=f"🕷 ¦𝙽𝙰𝙼𝙴 :{m.first_name}\n🐉 ¦𝚄𝚂𝙴𝚁 :@{m.username}\n🐰 ¦𝙸𝙳 :`{m.id}`\n🎬 ¦𝙱𝙸𝙾 :{m.bio}\n💎 ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n🗿 ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :`{message.chat.id}`",reply_markup=key)
                  else:
                     return await message.reply("• " + member.user.mention)
                     
@@ -36,10 +38,10 @@ async def gak_owne(client: Client, message: Message):
    
 
    
-@app.on_message(command(["اسمي", "شنو اسمي"]) & filters.group )
+@app.on_message(command(["اسمي", "اسمي اي"]) & filters.group )
 async def vgdg(client: Client, message: Message):
     await message.reply_text(
-        f""" اسمك هو »»  `{message.from_user.mention()}`""") 
+        f"""❤️‍🔥 اسمك »»  {message.from_user.mention()}""") 
 
         
 
@@ -47,24 +49,12 @@ array = []
 @app.on_message(command(["@all", "تاك","تاك للكل"]) & ~filters.private)
 async def nummmm(client: app, message):
   if message.chat.id in array:
-     return await message.reply_text(f"**تم بدأ التاك الجماعي \n\n بواسطة ← ✧ ¦{message.from_user.mention}**")
-
-  dev = (OWNER_ID)
-  haya = (6275847466,6195765774)
-  get = await client.get_chat_member(message.chat.id, message.from_user.id)
-  if message.from_user.id in haya:
-         rotba= "مبرمج السورس" 
-  elif message.from_user.id in dev:
-         rotba = "مطور اساسي" 
-  elif get.status in [ChatMemberStatus.OWNER]:
-         rotba = "المالك"
-  elif get.status in [ChatMemberStatus.ADMINISTRATOR]:
-         rotba = "الادمن"     
+     return await message.reply_text("**التاك قيد التشغيل حالياً ،**")
   chek = await client.get_chat_member(message.chat.id, message.from_user.id)
-  if not chek.status in  [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-    await message.reply(f"**انت لست مشرف يا {message.from_user.mention}**")
+  if not chek.status in ["administrator", "creator"]:
+    await message.reply("**يجب انت تكون مشرف لاستخدام الامر 🐉**")
     return
-  await message.reply_text(f"**تم بدأ التاك الجماعي \n\n بواسطة ← {rotba}✧ ¦{message.from_user.mention} \n\n للايقاف اكتب وقف منشن او خلاص**")
+  await message.reply_text("**لايقاف الامر اضغط **\n /cancel او اكتب ايقاف")
   i = 0
   txt = ""
   zz = message.text
@@ -73,11 +63,11 @@ async def nummmm(client: app, message):
           photo = await client.download_media(photo_id)
           zz = message.caption
   try:
-   zz = zz.replace("@all","").replace("تاك","").replace("صيحهم","")
+   zz = zz.replace("@all","").replace("تاك","").replace("نادي الكل","")
   except:
     pass
   array.append(message.chat.id)
-  async for x in client.get_chat_members(message.chat.id):
+  async for x in client.iter_chat_members(message.chat.id):
       if message.chat.id not in array:
         return
       if not x.user.is_deleted:
@@ -102,28 +92,19 @@ async def nummmm(client: app, message):
   array.remove(message.chat.id)
 
 
-@app.on_message(command(["كافي", "/cancel","اكطع"]))
+@app.on_message(command(["ايقاف", "/cancel","بس منشن"]))
 async def stop(client, message):
-  dev = (OWNER_ID)
-  haya = (5012406813,5012406813)
-  get = await client.get_chat_member(message.chat.id, message.from_user.id)
-  if get.status in [ChatMemberStatus.ADMINISTRATOR]:
-         rotba = "الادمن"
-  elif get.status in [ChatMemberStatus.OWNER]:
-         rotba = "المالك"
-  elif message.from_user.id in haya:
-         rotba= "مبرمج السورس" 
-  elif message.from_user.id in dev:
-         rotba = "مطور اساسي"           
   chek = await client.get_chat_member(message.chat.id, message.from_user.id)
-  if not chek.status in  [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-    await message.reply(f"**انت لست مشرفا يا {message.from_user.mention}**")
+  if not chek.status in ["administrator", "creator"]:
+    await message.reply("**يجب انت تكون مشرف لاستخدام الامر 🐉")
     return
   if message.chat.id not in array:
-     await message.reply(f"**التاك متوقف فالاصل \n\n يا {message.from_user.mention}**")
+     await message.reply("**المنشن متوقف بالفعل**")
      return 
   if message.chat.id in array:
     array.remove(message.chat.id)
-    await message.reply(f"**تم ايقاف التاك الجماعي \n\n بواسطة ← {rotba}✧ ¦{message.from_user.mention}**")
+    await message.reply("**تم ايقاف المنشن بنجاح*\n√*")
     return
+
+
 
