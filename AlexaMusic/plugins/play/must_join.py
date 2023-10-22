90e876bac7c31813197f489d117d75317693eb49
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-10-22 15:39:32 +0300

    Create must_join.py

---
+++ AlexaMusic/plugins/play/must_join.py
@@ -0,0 +1,36 @@
+from config import MUST_JOIN
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+
+
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: Client, msg: Message):
+    if not MUST_JOIN:
+        return
+    try:
+        try:
+            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await bot.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply_photo(
+                    photo="https://telegra.ph/file/78939fd6b9d5ffe28de02.jpg", caption=f"** اهلا بك عزيزي لا يمكنك استخدام البوت الى ب الاشتراك بالقناة اضغط على الزر في الاسفل** : /stert",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("‹ سورس سبارك ›", url=link),
+                            ]
+                        ]
+                    )
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"Promote me as an admin in the MUST_JOIN chat : {MUST_JOIN} !")
