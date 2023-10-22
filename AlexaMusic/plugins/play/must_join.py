5af55997b55ac157d3a52389beb542321c09679e
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-10-22 15:43:22 +0300

    Update must_join.py

--- AlexaMusic/plugins/play/must_join.py
+++ AlexaMusic/plugins/play/must_join.py
@@ -1,5 +1,3 @@
-from config import MUST_JOIN
-
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
