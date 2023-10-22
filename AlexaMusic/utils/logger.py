f7ea0e6798434a0d738e14114f01b43a4a421f39
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-10-22 20:15:37 +0300

    Update logger.py

--- AlexaMusic/utils/logger.py
+++ AlexaMusic/utils/logger.py
@@ -18,24 +18,24 @@ async def play_logs(message, streamtype):
         if message.chat.username:
             chatusername = f"@{message.chat.username}"
         else:
-            chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+            chatusername = "محادثه خاصه"
         logger_text = f"""
 **━━━━━━━━━━━━━━━**
-**💞 {MUSIC_BOT_NAME} ᴍᴜsɪᴄ ʟᴏɢs **
+**💞 {MUSIC_BOT_NAME} سجل التشغيل **
 **━━━━━━━━━━━━━━━**
-**🌹️ 𝐂𝐡𝐚𝐭 𝐍𝐚𝐌𝐞 : >** {message.chat.title} [`{message.chat.id}`]
+** اسم المجموعة : >** {message.chat.title} [`{message.chat.id}`]
 **━━━━━━━━━━━━━━━**
-**🥀 𝐍𝐚𝐌𝐞 : ›** {message.from_user.mention}
+** اسم المستخدم : ›** {message.from_user.mention}
 **━━━━━━━━━━━━━━━**
-**🌸 𝐔𝐬𝐞𝐑𝐧𝐚𝐌𝐞 : ›** @{message.from_user.username}
+** يوزر المستخدم : ›** @{message.from_user.username}
 **━━━━━━━━━━━━━━━**
-**🌷 𝐈𝐃  : ›** `{message.from_user.id}`
+** ايدي المستخدم : ›** `{message.from_user.id}`
 **━━━━━━━━━━━━━━━**
-**🌿 𝐂𝐡𝐚𝐭 𝐥𝐢𝐧𝐤: >** {chatusername}
+** رابط الكروب : >** {chatusername}
 **━━━━━━━━━━━━━━━**
-**🌻 𝐒𝐞𝐀𝐫𝐜𝐇𝐞𝐝 𝐅𝐨𝐫:** {message.text}
+** المطلوب :** {message.text}
 **━━━━━━━━━━━━━━━**
-**💐 𝐒𝐭𝐫𝐄𝐚𝐦 𝐓𝐲𝐏𝐞:** {streamtype}
+**نوع التشغيل :** {streamtype}
 **━━━━━━━━━━━━━━━**"""
         if message.chat.id != LOG_GROUP_ID:
             try:
