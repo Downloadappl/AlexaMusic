91d0f5cb82a542cd34cc52bb0cd62a3e21f5ebff
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-10-22 05:12:26 +0300

    Update id.py

--- AlexaMusic/plugins/play/id.py
+++ AlexaMusic/plugins/play/id.py
@@ -57,7 +57,7 @@ async def iddd(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        name, url=f"https://t.me/{message.from_user.username}")
+                        name, url=f"[{random_member.user.first_name}](tg://user?id={random_member.user.id})")
                 ],
             ]
         ),
