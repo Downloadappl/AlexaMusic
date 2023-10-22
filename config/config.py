564ec27259648db557fe045b5088f54c98db7b8b
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-10-22 15:41:28 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -46,6 +46,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/IIIlIIv")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/h_m_dr")
+MUST_JOIN = getenv("MUST_JOIN", None)
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
