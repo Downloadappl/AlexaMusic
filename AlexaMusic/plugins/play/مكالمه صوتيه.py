51246c92157253b9441f216ce2a31b9df445f390
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-10-23 13:57:44 +0300

    Update مكالمه صوتيه.py

--- AlexaMusic/plugins/play/مكالمه صوتيه.py
+++ AlexaMusic/plugins/play/مكالمه صوتيه.py
@@ -4,7 +4,7 @@ import asyncio
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from AlexaMusic.core.call import Anon
+from AlexaMusic.core.call import Alexa
 from AlexaMusic.utils.database import *
 from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJoinedError)
 
