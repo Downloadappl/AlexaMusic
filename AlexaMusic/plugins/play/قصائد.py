f950ca5b63b1061bbc0a1ac73264308a8fb68520
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-10-24 04:25:38 +0300

    Create قصائد.py

---
+++ AlexaMusic/plugins/play/قصائد.py
@@ -0,0 +1,32 @@
+import asyncio
+
+import os
+import time
+import requests
+from config import START_IMG_URL
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from AlexaMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AlexaMusic import app
+from random import  choice, randint
+
+
+
+@app.on_message(command(["ق", "حسينيات", "قصائد", "قصيده"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,90)
+    url = f"https://t.me/m_alkhaqanil/{rl}"
+    await client.send_voice(message.chat.id,url,caption="**‹ : تم اختيار قصيده حسينية لك : ›**",parse_mode="html",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
+
