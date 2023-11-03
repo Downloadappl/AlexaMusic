3787678dba783b20335e5db5932376a41a2e5108
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-11-04 00:14:33 +0300

    Update id.py

--- AlexaMusic/plugins/play/id.py
+++ AlexaMusic/plugins/play/id.py
@@ -135,7 +135,7 @@ async def khalid(client: Client, message: Message):
 
                     InlineKeyboardButton(
 
-                        name, user_id=from_user.id)
+                        name, user_id=5012406813)
 
                 ],
 
