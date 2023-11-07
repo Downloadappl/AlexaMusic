b207cf2229059d2cbd72266f0887c40409aad1d6
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-11-07 11:25:09 +0300

    Update settings.py

--- AlexaMusic/utils/inline/settings.py
+++ AlexaMusic/utils/inline/settings.py
@@ -30,7 +30,7 @@ def setting_markup(_):
         ],
         [
             InlineKeyboardButton(
-                text="‹ السورس ›",
+                text="",
                 url="https://t.me/ZZZ7iZ",
             ),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
