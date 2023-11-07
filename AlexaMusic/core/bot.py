3b78b2fc664327448cae4d613b2173b33ae7c606
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-11-07 11:17:30 +0300

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -20,7 +20,7 @@ from ..logging import LOGGER
 
 class AlexaBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"**• البوت بدا بالعمل الان ✅**")
         super().__init__(
             "MusicBot",
             api_id=config.API_ID,
@@ -35,19 +35,19 @@ class AlexaBot(Client):
         self.id = get_me.id
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "» ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ, ᴡᴀɪᴛɪɴɢ ғᴏʀ ᴀssɪsᴛᴀɴᴛ..."
+                config.LOG_GROUP_ID, "**- تم بدء البوت بنجاح الان**"
             )
         except:
             LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                "فشل الروبوت في الوصول إلى مجموعة السجل. تأكد من إضافة الروبوت الخاص بك إلى قناة السجل الخاصة بك وترقيته كمسؤول"
             )
             sys.exit()
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != "administrator":
-            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
+            LOGGER(__name__).error("يرجى رفع البوت مشرف في مجموعة السجل")
             sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        LOGGER(__name__).info(f"بدأ البوت كـ {self.name}")
