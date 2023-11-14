d32ab5c5a74b9000e529f4a43eb8698f4deaf695
ZZZ7iZ <131855150+ZZZ7iZ@users.noreply.github.com>
2023-11-14 14:42:53 +0300

    Update play.py

--- AlexaMusic/plugins/play/play.py
+++ AlexaMusic/plugins/play/play.py
@@ -7,7 +7,9 @@ from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto,
                             Message)
 from pytgcalls.exceptions import NoActiveGroupCall
 
-
+import config
+from config import (BANNED_USERS, lyrical, YAFA_NAME,
+                    YAFA_CHANNEL, CHANNEL_SUDO)
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
@@ -27,12 +29,29 @@ from AlexaMusic.utils.inline.playlist import botplaylist_markup
 from AlexaMusic.utils.logger import play_logs
 from AlexaMusic.utils.stream.stream import stream
 
+force_btn = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(   
+              text=f"{YAFA_NAME}", url=f"{YAFA_CHANNEL}",)                        
+        ],        
+    ]
+)
+async def check_is_joined(message):    
+    try:
+        userid = message.from_user.id
+        status = await app.get_chat_member(f"{CHANNEL_SUDO}", userid)
+        return True
+    except Exception:
+        await message.reply_text("**◇︰ عذرا، عليك الانضمام الى قناة البوت أولاً :**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        return False
+
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
           
 
 @app.on_message(
-    command(["","","شغل","تشغيل"])
+    command(["","قناه","شغل","تشغيل"])
     & ~filters.private
     & ~BANNED_USERS   
    
