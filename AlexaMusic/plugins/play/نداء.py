bd7a97a8443de92b764c95576e9f180b61f038cf
ZZZ7iZ <131855150+ZZZ7iZ@users.noreply.github.com>
2023-12-30 06:37:18 +0300

    Update نداء.py

--- AlexaMusic/plugins/play/نداء.py
+++ AlexaMusic/plugins/play/نداء.py
@@ -40,28 +40,3 @@ def call_random_member(client, message):
         f"**⌔︙الف مبروك تم زواجك من :** \n {random_member_mention} \n **"
     ])
     client.send_message(chat_id, random_message, reply_to_message_id=message.message_id, parse_mode='markdown')
-
-
-
-@app.on_message(filters.member_joined)
-async def get_chat_info(chat, already=False):
-    if not already:
-        chat = await app.get_chat(chat)
-    chat_id = chat.id
-    members = chat.members_count
-	await message.reply_text(f"""
-● نورت عمري ♥♡
-{message.from_user.first_name}
-●  يجب احترام الادمنية
-●  الالتزام بالقوانين في الوصف
-●  الاعضاء  {members} 
-""")
-
-
-@app.on_message(filters.member_left)
-async def leftmem(chat):
-    await message.reply_text(f"""
-    - الاسم «{ message.from_user.first_name}» 
-    
- - قام بغادرة المجموعة الان
-""")
