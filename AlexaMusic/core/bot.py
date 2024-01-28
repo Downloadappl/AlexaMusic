5b85cd4e2ffa94064fd7e2a9b9e2504341417a3b
ZZZ7iZ <131855150+ZZZ7iZ@users.noreply.github.com>
2024-01-29 01:37:25 +0300

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -1,53 +1,55 @@
-#
-# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
 
-# Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
-# Harshit Sharma
-# All rights reserved. © Alexa © Yukki
+""""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
 
 
 import sys
 
 from pyrogram import Client
-
 import config
-
 from ..logging import LOGGER
+from pyrogram.enums import ChatMemberStatus
 
 
 class AlexaBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"**• البوت بدا بالعمل الان ✅**")
         super().__init__(
             "MusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
+            in_memory=True,
         )
+        LOGGER(__name__).info(f"البوت يعمل بنجاح ...")
 
     async def start(self):
         await super().start()
         get_me = await self.get_me()
         self.username = get_me.username
         self.id = get_me.id
+        self.mention = get_me.mention
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "**- تم بدء البوت بنجاح الان**"
+                config.LOG_GROUP_ID, "» ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ, ᴡᴀɪᴛɪɴɢ ғᴏʀ ᴀssɪsᴛᴀɴᴛ..."
             )
         except:
             LOGGER(__name__).error(
-                "فشل الروبوت في الوصول إلى مجموعة السجل. تأكد من إضافة الروبوت الخاص بك إلى قناة السجل الخاصة بك وترقيته كمسؤول"
+                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
             )
             sys.exit()
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-        if a.status != "administrator":
-            LOGGER(__name__).error("يرجى رفع البوت مشرف في مجموعة السجل")
+        if a.status != ChatMemberStatus.ADMINISTRATOR:
+            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
             sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"بدأ البوت كـ {self.name}")
+        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
