c2bdb0d1776d66ebee9c936a0fce589223dc2d9d
ZZZ7iZ <131855150+ZZZ7iZ@users.noreply.github.com>
2024-01-29 01:40:00 +0300

    Update mongo.py

--- AlexaMusic/core/mongo.py
+++ AlexaMusic/core/mongo.py
@@ -1,7 +1,13 @@
-#
-# Copyright (C) 2021-2022 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-# Subscribe On YT Jankari Ki Duniya
+# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+
+""""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
 
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
@@ -35,3 +41,8 @@ else:
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
     mongodb = _mongo_async_.Alexa
     pymongodb = _mongo_sync_.Alexa
+
+## Database For Broadcast Subscription By Team Alexa
+
+MONGODB_CLI = MongoClient(config.MONGO_DB_URI)
+db = MONGODB_CLI["subscriptions"]
